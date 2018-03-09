c3f64ba1c258620a3ed3baa128f10e4e5a19185b
Michael Ganesan <cookieganesan@gmail.com>
2018-03-09 14:05:44 -0500

    Sample Files for RxJS Observer

---
+++ dist/createObserver.js
@@ -0,0 +1,22 @@
+"use strict";
+//This is an example of creating an observable
+//Do not import the entire library, this is a total waste of memory
+Object.defineProperty(exports, "__esModule", { value: true });
+const Observable_1 = require("rxjs/Observable");
+const colors = require("colors");
+const observable = Observable_1.Observable.create((observer) => {
+    //push items
+    observer.next("Smion");
+    observer.next("Jen");
+    observer.next("sergi");
+    observer.next("Michael Ganesan");
+    observer.complete(); // list is done
+});
+//subscribe to the list
+observable.subscribe((x) => {
+    console.log(x);
+}, (error) => {
+    console.log(`An error has occured with the stream ${error}`);
+}, () => {
+    console.log(colors.yellow("Process completed"));
+});
---
+++ src/createObserver.ts
@@ -0,0 +1,32 @@
+//This is an example of creating an observable
+//Do not import the entire library, this is a total waste of memory
+
+import { Observable } from 'rxjs/Observable';
+
+
+import * as colors from 'colors';
+
+const observable = Observable.create((observer:any) =>{
+   //push items
+   observer.next("Smion");
+   observer.next("Jen");
+   observer.next("sergi");
+   observer.next("Michael Ganesan");
+   observer.complete(); // list is done
+});
+
+
+//subscribe to the list
+observable.subscribe((x:any)=>{
+     console.log(x);
+
+},(error)=>{
+  console.log(`An error has occured with the stream ${error}`);
+},
+()=>{
+   console.log(colors.yellow("Process completed"));
+
+});
+
+
+
